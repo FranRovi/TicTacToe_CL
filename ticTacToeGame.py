ab221f05ee9c8f3fee88dec697694f7a1bde8acf
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2024-01-23 10:20:24 -0800

    Update isGameOver and validateMove functions. WIP

--- ticTacToeGame.py
+++ ticTacToeGame.py
@@ -1,8 +1,21 @@
 # WIP
-def validateMove():
+def move():
     pass
 
-def updateActivePlayer():
+def validateMove(board, square):
+    if square < 1 or square > 9:
+        print("Invalid Move: Square selected is out of range. Try again!.")
+    if board[square] != '?':
+        print("Invalid Move: Square selected is already occupied. Try again!")  
+
+def updateActivePlayer(currentPlayer):
+    # if currentPlayer == 1:
+    #     return currentPlayer + 1
+    # else:
+    #     return currentPlayer - 1
+    return currentPlayer + 1 if currentPlayer == 1 else currentPlayer - 1
+
+def updatePlayerLastMoves(player):
     pass
 
 def updateBoard(board, moves1, moves2):
@@ -11,7 +24,16 @@ def updateBoard(board, moves1, moves2):
         board[moves2[i]] = "O"
     return board
 
-def isWinner():
+def isGameOver(currentPlayer):
+    winningMoves = [
+        (1,2,3),(4,5,6),(7,8,9),(1,4,7),
+        (2,5,8),(3,6,9),(3,5,7),(1,5,9),
+    ]
+    if currentPlayer in winningMoves:
+        print("Winner Winner chiken dinner")
+        return True
+
+def getWinner():
     pass
 
 def printBoard(board):    
@@ -36,11 +58,22 @@ def main():
         8:"?",
         9:"?"
     }
-    player1 = [1,5,7]
-    player2 = [3,8,9]
+    printBoard(initialBoard)
+    currentPlayer = 1
+    player1 = (3,8,6)
+    player2 = (1,5,9)
+    
     currentBoard = updateBoard(initialBoard, player1, player2)
     # print(currentBoard)
     printBoard(currentBoard)
+    currentPlayer = updateActivePlayer(currentPlayer)
+    print(currentPlayer)
+    currentPlayer = updateActivePlayer(currentPlayer)
+    print(currentPlayer)
+    currentPlayer = updateActivePlayer(currentPlayer)
+    print(currentPlayer)
+    print(isGameOver(player2))
+    
 
 if __name__ == "__main__":
     main()
